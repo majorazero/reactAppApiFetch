c16eaaafc5b6a460456fddee24ba52bb76775165
majorazero <daniel.cw.hsu@gmail.com>
2018-03-05 22:05:14 -0800

    figured out how to fetcha data in REACT

--- component.js
+++ component.js
@@ -4,6 +4,22 @@ class Page extends React.Component{
       <div className="intro">Hello World!</div>
     );
   }
+  componentDidMount(){
+    console.log(2);
+    this._weatherApiCall();
+  }
+  _weatherApiCall(){
+    let apiKey = "c033c7d88ddd656c159ed45f9a39923e";
+    let city = "London";
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    fetch(url)
+    .then(response => {
+      response.json()
+      .then( myJson => {
+        console.log(myJson.name);
+      });
+    });
+  }
 }
 
 jQuery(function(){
