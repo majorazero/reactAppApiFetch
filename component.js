1923bf3cc26427fdd6f5456ad61f9949251080d0
majorazero <daniel.cw.hsu@gmail.com>
2018-03-20 16:14:36 -0700

    removed htt=http-server and start module and installed express, going to refactor everything

--- component.js
+++ component.js
@@ -1,4 +1,10 @@
 class Page extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+
+    };
+  }
   render(){
     return(
       <div className="intro">Hello World!</div>
@@ -6,7 +12,21 @@ class Page extends React.Component{
   }
   componentDidMount(){
     console.log(2);
-    this._weatherApiCall();
+    this._spotifyApiCallAuthorize();
+  }
+  _spotifyApiCallAuthorize(){
+    let client_id = "13e2291fbf6f457a802925ffb8e13fa8";
+    let client_secret = "0af4182b39ed4e16bee0ddaf61480667";
+    let redirect = "localhost:8000";
+    let url =
+    `https://api.spotify.com/authorize/?client_id=${client_id}&response_type=code&redirect_uri=${redirect}`;
+    console.log(url);
+    fetch(url)
+      .then(response => {
+        console.log(response);
+      }).then(myJson => {
+        console.log(myJson);
+      });
   }
   _weatherApiCall(){
     let apiKey = "c033c7d88ddd656c159ed45f9a39923e";
@@ -21,7 +41,6 @@ class Page extends React.Component{
     });
   }
 }
-
 jQuery(function(){
   ReactDOM.render(
     <Page />,
