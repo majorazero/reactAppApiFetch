aa65750efee0d34fb149190a861116e8bfef1f20
majorazero <daniel.cw.hsu@gmail.com>
2018-03-20 16:55:12 -0700

    removed unneccesary code and shrunkwrap everything

--- public/component.js
+++ public/component.js
@@ -10,24 +10,6 @@ class Page extends React.Component{
       <div className="intro">Hello World!</div>
     );
   }
-  componentDidMount(){
-    console.log(2);
-    //this._spotifyApiCallAuthorize();
-  }
-  _spotifyApiCallAuthorize(){
-    let client_id = "13e2291fbf6f457a802925ffb8e13fa8";
-    let client_secret = "0af4182b39ed4e16bee0ddaf61480667";
-    let redirect = "localhost:8000";
-    let url =
-    `https://api.spotify.com/authorize/?client_id=${client_id}&response_type=code&redirect_uri=${redirect}`;
-    console.log(url);
-    fetch(url)
-      .then(response => {
-        console.log(response);
-      }).then(myJson => {
-        console.log(myJson);
-      });
-  }
   _weatherApiCall(){
     let apiKey = "c033c7d88ddd656c159ed45f9a39923e";
     let city = "London";
--- test.js
+++ test.js
@@ -9,4 +9,9 @@ describe("Requests to the root path",function(){
       .get("/")
       .expect(200,done);
   });
+  it("Returns html element",function(done){
+    request(app)
+    .get("/")
+    .expect("Content-type",/html/,done);
+  });
 });
